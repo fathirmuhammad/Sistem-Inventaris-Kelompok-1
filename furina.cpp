629942bdd52212c688e39e537c5886cb6a940272
AghisaChristyananda <aghisachristynanda434@gmail.com>
2024-10-08 11:03:16 +0700

    Update furina.cpp

--- furina.cpp
+++ furina.cpp
@@ -18,3 +18,25 @@ int jumlah;
 
     barang->nama = nama;
     barang->jumlah = jumlah;
+}
+
+void inputBarang (Barang barang) {
+    cout << " nama barang = " << barang.nama << endl;
+    cout << " jumlah barang = " << barang.jumlah << endl;
+
+}
+
+int main () {
+   Barang daftarBarang[10];
+   int jumlahBarang = 0;
+   int pilihan;
+
+   do{
+      cout << "=== Inventaris Barang ===" << endl;
+      cout << "1. Tambahkan barang" << endl;
+      cout << "2. Tampilkan barang" << endl;
+      cout << "3. Selesai" << endl;
+      cout << "Pilih opsi: ";
+      cin >> pilihan;
+
+ng->jumlah = jumlah;
